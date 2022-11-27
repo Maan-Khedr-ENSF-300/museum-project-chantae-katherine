760fb572ea9632e7c0489888123f16d753c8f60c
chantaeh <96901376+chantaeh@users.noreply.github.com>
2022-11-27 13:45:44 -0700

    Change PAINTING Style attribute name

--- sql scripts folder/museum.sql
+++ sql scripts folder/museum.sql
@@ -65,7 +65,7 @@ CREATE TABLE ON_DISPLAY (
 DROP TABLE IF EXISTS PAINTING;
 CREATE TABLE PAINTING (
     Id_no               INT NOT NULL,
-    Paint_style         VARCHAR(25),
+    Style               VARCHAR(25),
     Drawn_on            VARCHAR(20),
     Paint_type          VARCHAR(20),
     PRIMARY KEY (Id_no),
