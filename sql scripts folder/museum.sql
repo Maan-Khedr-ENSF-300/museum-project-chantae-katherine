e5cc0de9f9b4c111b83e94bf966f06a3db565cd6
chantaeh <96901376+chantaeh@users.noreply.github.com>
2022-11-27 12:25:56 -0700

    Add files via upload

---
+++ sql scripts folder/museum.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS MUSEUM;
+CREATE DATABASE MUSEUM; 
+USE MUSEUM;
+
+DROP TABLE IF EXISTS EXHIBITION;
+CREATE TABLE EXHIBITION (
+	E_id				INT NOT NULL,
+    EName				VARCHAR(30) NOT NULL,
+    Start_date			DATE,
+    End_date			DATE,
+    PRIMARY KEY (E_id)
+);
+
+DROP TABLE IF EXISTS ARTIST;
+CREATE TABLE ARTIST (
+	FName				VARCHAR(20) NOT NULL,
+    LName				VARCHAR(20) NOT NULL,
+    Main_style			VARCHAR(25),
+    Artist_descrip		VARCHAR(250),
+    Date_born			DATE,
+    Date_died			DATE,
+    Epoch				VARCHAR(20),
+	Country_of_origin	VARCHAR(30),
+	PRIMARY KEY (FName, LName)
+);
