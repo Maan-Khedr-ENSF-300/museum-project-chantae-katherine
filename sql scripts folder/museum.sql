d71f1ed42c32f13c5d5aa4fc2e5991baa096bd71
chantaeh <96901376+chantaeh@users.noreply.github.com>
2022-11-28 15:32:08 -0700

    Insert more records

--- sql scripts folder/museum.sql
+++ sql scripts folder/museum.sql
@@ -33,14 +33,14 @@ INSERT INTO ARTIST(FName, LName, Main_style, Artist_descrip, Date_born, Date_die
 VALUES
 ("Vivian", "Maier", "Black & White", "Vivian Dorothy Maier was an American street photographer whose 
 work was discovered and recognized after her death. She worked for about 40 years as a nanny, mostly 
-in Chicago's North Shore, while pursuing photography.", "1926-02-01", "2009-04-21", "Modern", "American"),
+in Chicago's North Shore, while pursuing photography.", "1926-02-01", "2009-04-21", "Modern", "United States"),
 ("Dempsey", "Bob", "Traditional", "Dempsey Bob, OC D.Litt is a Northwest Coast woodcarver and 
 sculptor from British Columbia, Canada, who is of Tahltan and Tlingit First Nations descent. He was 
 born in the Tahltan village of Telegraph Creek on the Stikine River in northwestern B.C., and is of the Wolf clan. ",
-"1948-01-01", null, "Contemporary", "Tahltan-Tlingit"),
+"1948-01-01", null, "Contemporary", "Canada"),
 ("Lai Cheuk", "Wah", "Realistic", "Sarah Lai Cheuk Wah is best known for her paintings of common objects and 
 urban landscapes, which she renders realistically in great detail. Her visual language is derived from the 
-ordinary, the mundane and the everyday.", "1983-01-01", null, "Contemporary", "Hong Konger");
+ordinary, the mundane and the everyday.", "1983-01-01", null, "Contemporary", "Hong Kong");
 
 
 DROP TABLE IF EXISTS COLLECTION;
@@ -72,7 +72,7 @@ CREATE TABLE ART_OBJECT (
     Artist_FName        VARCHAR(20), 
     Artist_LName        VARCHAR(20),
     Year_created        INTEGER,
-    Title               VARCHAR(30),
+    Title               VARCHAR(50),
     Origin              VARCHAR(30),
     ArtObj_descrip      VARCHAR(300),
     Art_type            VARCHAR(20) NOT NULL DEFAULT 'OTHER',
@@ -84,6 +84,17 @@ CREATE TABLE ART_OBJECT (
     FOREIGN KEY (Artist_FName, Artist_LName) REFERENCES ARTIST(FName, LName),
     FOREIGN KEY (Borrowed_collection) REFERENCES COLLECTION(Name)
 );
+
+INSERT INTO ART_OBJECT(Id_no, Artist_FName, Artist_LName, Year_created, Title, Origin, 
+ArtObj_descrip, Art_type, Epoch, Borrowed_collection, Date_borrowed, Date_returned)
+VALUES
+(1, "Vivian", "Maier", 1953, null, "American", "From Street Photography 1", "OTHER", "Modern", "Glenbow Collection", "2020-01-02", "2020-06-15"),
+(2, "Dempsey", "Bob", null, "Traders of the Sea", "Canadian", "5' x 10' Wood panel", "OTHER", "Contemporary", null, null, null),
+(3, "Lai Cheuk", "Wah", 2013, "Spotting the Light onto a Light", "Hong Konger", null, "PAINTING", "Contemporary", "Met Collection", "2022-05-06", null),
+(4, "Lai Cheuk", "Wah", 2013, "Sugar Rock", "Hong Konger", null, "PAINTING", "Contemporary", "Met Collection", "2022-05-06", null),
+(5, "Lai Cheuk", "Wah", 2009, "Sea (night series)", null, "OTHER", "Contemporary", null, null, null),
+(6, "Lai Cheuk", "Wah", 2001, "A Drowsy Car", null, "PAINTING", "Contemporary", null, null, null);
+
 
 DROP TABLE IF EXISTS ON_DISPLAY;
 CREATE TABLE ON_DISPLAY (
@@ -103,6 +114,12 @@ CREATE TABLE PAINTING (
     PRIMARY KEY (Id_no),
     FOREIGN KEY (Id_no) REFERENCES ART_OBJECT(Id_no)
 );
+
+INSERT INTO PAINTING(Id_no, Style, Drawn_on, Paint_type)
+VALUES
+(3, "Realistic", "Canvas", "Oil");
+(4, "Realistic", "Canvas", "Oil");
+(6, "Realistic", "Canvas", "Oil");
 
 DROP TABLE IF EXISTS SCULPTURE;
 CREATE TABLE SCULPTURE (
@@ -130,10 +147,16 @@ DROP TABLE IF EXISTS OTHER;
 CREATE TABLE OTHER (
     Id_no               INT NOT NULL,
     Style               VARCHAR(20),
-    Type                VARCHAR(20),
+    Type                VARCHAR(30),
     PRIMARY KEY (Id_no),
     FOREIGN KEY (Id_no) REFERENCES ART_OBJECT(Id_no)
 );
+
+INSERT INTO OTHER(Id_no, Style, Type)
+VALUES
+(1, "Black & White", "Photo"),
+(2, "Traditional", "Wood panel"),
+(5, "Seascape", "Silverpoint on wood panel");
 
 DROP TABLE IF EXISTS IN_PERMANENT_COLLECTION;
 CREATE TABLE IN_PERMANENT_COLLECTION (
