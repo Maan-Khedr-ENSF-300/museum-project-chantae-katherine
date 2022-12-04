890edc9d8a5dcd8f7c7f8d42f7716ddc5fd80aab
chantaeh <96901376+chantaeh@users.noreply.github.com>
2022-12-04 09:44:54 -0700

    fix collection description formatting

--- sql scripts folder/museum.sql
+++ sql scripts folder/museum.sql
@@ -58,15 +58,14 @@ CREATE TABLE COLLECTION (
 
 INSERT INTO COLLECTION(Name, Contact_person, Type, C_descrip, Address, Phone)
 VALUES
-("Glenbow Collection", "Melanie Kjorlien", "Museum", "Glenbow\'s incredible collection of 
-art and objects can provide us with a sense of belonging, an understanding of what 
-it means to live here in Alberta, and an appreciation for the legacy of the remarkable 
-people and events that have shaped our communities.", "130 9 Ave SE, Calgary, AB",
+("Glenbow Collection", "Melanie Kjorlien", "Museum", "Glenbow\'s incredible collection of art and objects can provide us with a sense of 
+    belonging, an understanding of what it means to live here in Alberta, and an appreciation for the 
+    legacy of the remarkable people and events that have shaped our communities.", "130 9 Ave SE, Calgary, AB",
 "14032684100"),
-("Met Collection", "Tiarra Brown", "Museum", "The Metropolitan Museum of Art is one of the 
-world's largest and finest art museums. Its collections include more than two million works 
-of art spanning 5,000 years of world culture, from prehistory to the present and from every 
-part of the globe.", "1000 Fifth Avenue, New York, NY", "12125357710");
+("Met Collection", "Tiarra Brown", "Museum", 
+"The Metropolitan Museum of Art is one of the world's largest and finest art museums. Its collections include 
+    more than two million works of art spanning 5,000 years of world culture, from prehistory to the present 
+    and from every part of the globe.", "1000 Fifth Avenue, New York, NY", "12125357710");
 
 DROP TABLE IF EXISTS ART_OBJECT;
 CREATE TABLE ART_OBJECT (
